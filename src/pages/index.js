3f38b77ff1bbe948544aca6c4a906e5d2b680114
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-08-22 16:48:24 +0300

    Adding pages and imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as  Customers } from './Customers'
 export { default as Orders } from './Orders'
 export { default as Employees } from './Employees'
+export { default as Ecommerce } from './Ecommerce'
